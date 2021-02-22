b08b91ace955c827256b8851993f12b361aa999f
tianfeng <tianfengseis@ucla.edu>
2021-02-21 21:47:19 -0800

    dasd

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ function setImage(select){
   var image = document.getElementsByName("image-swap")[0];
   image.src = select.options[select.selectedIndex].value;
 }
+
+function setHTML(select){
+  var html = document.getElementsByName("html-swap")[0];
+  html.src = select.options[select.selectedIndex].value;
+}
