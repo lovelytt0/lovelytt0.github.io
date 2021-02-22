c6299fbc6aefbe90ce2335e493459bd7846cb401
tianfeng <tianfengseis@ucla.edu>
2021-02-21 20:52:41 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -146,3 +146,9 @@ self.addEventListener('fetch', event => {
     );
   }
 });
+
+
+function setImage(select){
+    var image = document.getElementsByName("image-swap")[0];
+    image.src = select.options[select.selectedIndex].value;
+}
